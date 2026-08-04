a0efa97e7d7b992314ffa264eff321e0afea1d76
XZer0 <83530028+jonrlstockwell@users.noreply.github.com>
2026-08-04 17:35:00 -0600

    Load Developer Dashboard module

--- tactic.user.js
+++ tactic.user.js
@@ -41,6 +41,7 @@
 // @require      https://raw.githubusercontent.com/jonrlstockwell/TACTIC/main/ui/drawer/index.js?v=3
 //
 // @require      https://raw.githubusercontent.com/jonrlstockwell/TACTIC/main/modules/demo/index.js?v=3
+// @require      https://raw.githubusercontent.com/jonrlstockwell/TACTIC/main/modules/developer/index.js?v=1
 //
 // @require      https://raw.githubusercontent.com/jonrlstockwell/TACTIC/main/app/bootstrap.js?v=4
 //
