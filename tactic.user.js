1c02677f8aed65b287129cc3573e5aee8e5f97b8
XZer0 <83530028+jonrlstockwell@users.noreply.github.com>
2026-08-05 17:16:05 -0600

    Remove abandoned Page Object service

--- tactic.user.js
+++ tactic.user.js
@@ -60,14 +60,6 @@
 //
 //
 // ============================================================
-// PAGE OBJECTS
-// ============================================================
-//
-// @require      https://raw.githubusercontent.com/jonrlstockwell/TACTIC/main/services/pages/index.js?v=3
-// @require      https://raw.githubusercontent.com/jonrlstockwell/TACTIC/main/services/pages/faction-bank.js?v=3
-//
-//
-// ============================================================
 // GENERAL APPLICATION SERVICES
 // ============================================================
 //
