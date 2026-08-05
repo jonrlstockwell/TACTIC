0a8108a23f6b8c9c00914d1b7a405ec2307b343e
XZer0 <83530028+jonrlstockwell@users.noreply.github.com>
2026-08-04 18:05:00 -0600

    Update

--- tactic.user.js
+++ tactic.user.js
@@ -44,7 +44,7 @@
 //
 // @require      https://raw.githubusercontent.com/jonrlstockwell/TACTIC/main/modules/demo/index.js?v=3
 // @require      https://raw.githubusercontent.com/jonrlstockwell/TACTIC/main/modules/developer/index.js?v=3
-// @require      https://raw.githubusercontent.com/jonrlstockwell/TACTIC/main/modules/developer/live-refresh.js?v=2
+// @require      https://raw.githubusercontent.com/jonrlstockwell/TACTIC/main/modules/developer/live-refresh.js?v=3
 //
 // @require      https://raw.githubusercontent.com/jonrlstockwell/TACTIC/main/app/bootstrap.js?v=4
 //
