42efb46e6716aba13180cc6401b35122cdaf6b03
XZer0 <83530028+jonrlstockwell@users.noreply.github.com>
2026-08-05 20:03:45 -0600

    Update

--- tactic.user.js
+++ tactic.user.js
@@ -8,6 +8,7 @@
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_deleteValue
+// @grant        GM_registerMenuCommand
 // @grant        unsafeWindow
 // @run-at       document-start
 
